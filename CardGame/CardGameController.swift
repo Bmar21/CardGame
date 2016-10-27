62d615c3f86eb55bd5cf6ca29c2c581cbe0376a3
Bmar21 <brandonmalor21@gmail.com>
2016-10-27 12:31:49 -0600

    added a care game controller class to replace the regular controller class

---
+++ CardGame/CardGameController.swift
@@ -0,0 +1,15 @@
+//
+//  CardGameController.swift
+//  CardGame
+//
+//  Created by Marlor, Brandon on 10/27/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class CardGameController :
+    UIViewController
+{
+    
+}
