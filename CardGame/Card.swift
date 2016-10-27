7b72292c77e9efab9c25cd795ae9a9a1091db2c8
Bmar21 <brandonmalor21@gmail.com>
2016-10-27 13:20:28 -0600

    added setters and getters for the card file in swift

--- CardGame/Card.swift
+++ CardGame/Card.swift
@@ -6,4 +6,70 @@
 //  Copyright © 2016 CTEC. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Card
+{
+    private var backImage : UIImage
+        {
+        get
+        {
+            return self.backImage
+        }
+        set(backImage)
+        {
+            self.backImage = backImage
+        }
+    }
+    
+    
+    
+    private var isFaceUp : Bool
+        {
+        get
+        {
+            return self.isFaceUp
+        }
+        set(isFaceUp)
+        {
+            self.isFaceUp = isFaceUp
+        }
+    }
+
+
+
+    init()
+    {
+        backImage = UIImage()
+        isFaceUp = false
+    }
+    
+    func setBackImage(backImage : UIImage)
+    {
+        self.backImage = backImage
+    }
+    
+    func getBackImage() -> UIImage
+    {
+        return backImage
+    }
+
+    func setFacing(isFaceUp : Bool)
+    {
+        self.isFaceUp = isFaceUp
+    }
+    
+    func isFacing() -> Bool
+    {
+        return isFaceUp
+    }
+    
+    
+    func toString () -> String
+    {
+        let description = "This cards \(isFaceUp)"
+        
+        return description
+    }
+
+}
