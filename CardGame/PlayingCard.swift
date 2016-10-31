5fdb89e810151c299b4a4e0f555444d0afc682bc
Bmar21 <brandonmalor21@gmail.com>
2016-10-31 12:34:36 -0600

    added getters and setters for PlayingCard

--- CardGame/PlayingCard.swift
+++ CardGame/PlayingCard.swift
@@ -6,4 +6,128 @@
 //  Copyright © 2016 CTEC. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class PlayingCard : Card
+{
+    private var rank : Int
+        {
+        get
+        {
+            return self.rank
+        }
+        set(rank)
+        {
+            self.rank = rank
+        }
+    }
+    
+    
+    Private var suit : String
+    {
+        get
+        {
+        return self.suit
+        }
+        
+        set(suit)
+        {
+            self.suit = suit
+        }
+    }
+    
+    private var color : UIColor
+    {
+        get
+        {
+        return self.color
+        }
+        
+        set(color)
+        {
+            self.color = color
+        }
+    }
+    
+    override init()
+    {
+        suit = ""
+        color = UIColor()
+        super.init()
+        rank = 0
+        
+    }
+    
+    func getrank() -> Int
+    {
+        return rank
+    }
+    
+    func getsuit() -> String
+    {
+        return suit
+    }
+    
+    func getcolor() -> UIColor
+    {
+        return color
+    }
+    
+    override func toString() -> String
+    {
+        let backStatus : String
+        if  super.isFacing()
+        {
+            backStatus = "is face up"
+        }
+        else
+        {
+            
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
