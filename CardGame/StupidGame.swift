193d01b4357ddab1718d627df9549fcdbd50937d
Bmar21 <brandonmalor21@gmail.com>
2016-11-30 13:42:17 -0700

    added to StupidGame

--- CardGame/StupidGame.swift
+++ CardGame/StupidGame.swift
@@ -6,4 +6,50 @@
 //  Copyright © 2016 CTEC. All rights reserved.
 //
 
-import Foundation
+class StupidGame
+{
+    internal var gameDeck : PlayingCardDeck
+    internal var hand : [PlayingCard]
+    internal var score : Int
+    
+    init()
+    {
+        gameDeck = PlayingCardDeck()
+        hand = [PlayingCard]()
+        score = Int(0)
+    }
+    
+    func startGame() -> Void
+    {
+        gameDeck.shuffleDeck()
+        drawCards()
+    }
+    
+    private func drawCards() -> Void
+    {
+        hand.append((gameDeck.drawCard() as? PlayingCard)!)
+        hand.append((gameDeck.drawCard() as? PlayingCard)!)
+    }
+    
+    func checkMatch() -> Bool
+    {
+        let hasMatch :Bool
+        if(hand[0].rank == hand[1].rank)
+    }
+    
+    func playGame() -> Void
+    {
+        if hand[0].rank > 5
+        {
+            score += hand[0].rank
+        }
+        else
+        {
+            score -= 10
+        }
+        hand.removeAtIndex(0) 
+        drawCards()
+    }
+
+}
+
